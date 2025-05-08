6cf131299365877125ddf03c4b68fb0842736ede
Aiman Hussain <108855291+AimanHussainWebTech@users.noreply.github.com>
2025-05-08 13:45:51 +0500

    web by subhan hammad and shibra

---
+++ web-frontend/src/App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+
+import Index from './Web_Components/Index';
+import Navbar from './Web_Components/Navbar';
+import Footer from './Web_Components/Footer';
+import Contact from './Web_Components/Contact';
+import About from './Web_Components/About';
+import Blog from './Web_Components/Blog';
+import Schedule from './Web_Components/Schedule';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+    <Navbar/>
+     <Routes>
+     
+      <Route path="/" element={<Index />}/>
+      {/* <Route path="/contact" element={<Contact />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/blog" element={<Blog />}/>
+      <Route path="/schedule" element={<Schedule />}/>
+       */}
+     </Routes>
+     <Footer/>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ web-frontend/src/Web_Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div> <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div className="container">
+      <a className="navbar-brand" href="index.html">Plataforma.</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="oi oi-menu"></span> Menu
+      </button>
+
+      <div className="collapse navbar-collapse" id="ftco-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active"><Link to="/" className="nav-link">Home</Link></li>
+          <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
+          <li className="nav-item"><Link to="/speakers" className="nav-link">Speakers</Link></li>
+          <li className="nav-item"><Link to="/schedule" className="nav-link">Schedule</Link></li>
+          <li className="nav-item"><Link to="/blog" className="nav-link">Blog</Link></li>
+          <li className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></li>
+          {/* <li className="nav-item cta mr-md-2"><a href="#" className="nav-link">Buy ticket</a></li> */}
+
+        </ul>
+      </div>
+    </div>
+  </nav></div>
+  )
+}
